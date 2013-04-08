5075746d3899d99ec0b7a684be4ab0c52ffc7bd7
Pete Khrapchenkov <shigidono@gmail.com>
2013-04-08 16:59:22 +1100

    Added test build configuration.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <spawner.h>
+#include <Windows.h>
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    char str[2048];
+    GetCurrentDirectory(2048, str);
+}
